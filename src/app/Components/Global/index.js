2114ffca610ac87639c5cfdb52182f1721a42587
Sensaz <klaudiusz.wiktor1@gmail.com>
2023-07-30 13:29:20 +0200

    [refactor] - update imports in auth folder

--- src/app/Components/Global/index.js
+++ src/app/Components/Global/index.js
@@ -1,4 +1,5 @@
 import { LoginPopUp, RegisterPopUp } from "./Authorized";
 import { Button } from "./Button";
+import { Portal } from "./Portal";
 
-export { LoginPopUp, RegisterPopUp, Button };
+export { LoginPopUp, RegisterPopUp, Button, Portal };
