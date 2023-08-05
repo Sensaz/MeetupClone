d6a14ea8d601f34abc6d43c3f6dda8a1d723ba8c
Sensaz <klaudiusz.wiktor1@gmail.com>
2023-08-05 19:08:57 +0200

    [feat] - Added Event & Group Card componend and styled it

--- src/app/find/page.tsx
+++ src/app/find/page.tsx
@@ -5,24 +5,44 @@ import React, { useEffect } from "react";
 
 import { EventsPage, GroupsPage } from "./components";
 import Link from "next/link";
+import classNames from "classnames";
+import "../../style/find/near-meetups-page.sass";
 
-export default function SourcePage() {
+export default function FindPage() {
   const searchParams = useSearchParams();
   const router = useRouter();
 
   const search = searchParams.get("source");
   useEffect(() => {
-    if (search !== 'GROUPS') router.push("?source=EVENTS");
+    if (search !== "GROUPS") router.push("?source=EVENTS");
   }, []);
 
   return (
-    <div>
-      <h1>
+    <div className="near-meetups-page">
+      <h1 className="near-meetups-page__header">
         {search === "GROUPS" ? "Grupy w pobliżu" : "Wydarzenia w pobliżu"}
       </h1>
-      <Link href="?source=EVENTS">Wydarzenia</Link>
-      <Link href="?source=GROUPS">Grupy</Link>
-      <main>{search === "GROUPS" ? <GroupsPage /> : <EventsPage />}</main>
+      <div className="near-meetups-page__swap-links">
+        <Link
+          className={classNames("nes-text nes-pointer", {
+            "is-disabled": search === "GROUPS",
+          })}
+          href="?source=EVENTS"
+        >
+          Wydarzenia
+        </Link>
+        <Link
+          className={classNames("nes-text nes-pointer", {
+            "is-disabled": search === "EVENTS",
+          })}
+          href="?source=GROUPS"
+        >
+          Grupy
+        </Link>
+      </div>
+      <div className="near-meetups-page__main">
+        {search === "GROUPS" ? <GroupsPage /> : <EventsPage />}
+      </div>
     </div>
   );
 }
